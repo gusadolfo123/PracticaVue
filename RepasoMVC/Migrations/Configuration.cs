8fd9d4a74fd1b906489fe179becc257af60f6d24
gusadolfo123 <gusadolfo123@hotmail.com>
2019-01-19 09:50:30 -0500

    Ajustes Iniciales

---
+++ RepasoMVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace RepasoMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RepasoMVC.Models.RepasoMVCContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RepasoMVC.Models.RepasoMVCContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ RepasoMVC/Models/RepasoMVCContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RepasoMVC.Models
+{
+    public class RepasoMVCContext: DbContext
+    {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public RepasoMVCContext(): base(nameof(RepasoMVCContext))
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Category>().HasKey(c => c.CategoryID);
+            modelBuilder.Entity<Category>().Property(c => c.Description).HasMaxLength(200);
+
+            modelBuilder.Entity<Product>().HasKey(p => p.ProductID);
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).HasMaxLength(80);
+            modelBuilder.Entity<Product>().HasIndex(p => new { p.ProductName, p.CategoryID }).IsUnique(true);
+
+            base.OnModelCreating(modelBuilder); 
+        }
+
+    }
+}
